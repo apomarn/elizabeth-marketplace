08656be86fb704dba1604d05907b3920a592fedd
Anghy <anghyf27@hotmail.com>
2019-07-12 11:26:34 -0500

    created Hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from '../src/pages/Navbar'
+import Hero from '../src/pages/Hero'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
