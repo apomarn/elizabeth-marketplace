73a895859648bbdbc3ff5af1791d40afd2c8b922
Anghy <anghyf27@hotmail.com>
2019-08-27 16:57:20 -0400

    header

--- src/pages/PopularSection/index.js
+++ src/pages/PopularSection/index.js
@@ -5,12 +5,16 @@ import { colors, textStyles } from '../../styles'
 const Header = styled.p`
   font-size: 28px;
   color: ${colors.gray};
+  text-align: left;
+  font-weight: 700;
+  margin-top: 100px;
+  margin-bottom: 0;
 `
 
 const Underline = styled.div`
   background-color: ${colors.gray};
-  height: 10px;
-  width: 40px;
+  height: 5px;
+  width: 455px;
 `
 
 class PopularSection extends Component {
